ca87e5e5f0d648d65dafc0f86e741ed504179cb1
Hyerim Han <hyerim_han@naver.com>
2022-09-09 13:09:38 +0900

    프로퍼티 어트리뷰트

---
+++ ex16.js
@@ -0,0 +1,50 @@
+// 프로퍼티 어트리뷰트
+// ex16-13
+// 불변 객체
+const person = {
+    name: 'Lee',
+    address: {city: 'Seoul'}
+};
+
+// 얕은 객체 동결
+Object.freeze(person);
+
+// 직속 프로퍼티만 동결한다.
+console.log(Object.isFrozen(person)); // true
+// 중첩 객체까지 동결하지 못한다.
+console.log(Object.isFrozen(person.address)); // false
+
+person.address.city = 'Busan';
+console.log(person); // {name: "Lee", address: {city: "Busan"}}
+
+
+
+// ex16-14
+function deepFreeze(target) {
+    // 객체가 아니거나 동결된 객체는 무시하고 객체이고 동결되지 않은 객체만 동결한다.
+    if(target && typeof target === 'object' && !Object.isFrozen(target)) {
+        Object.freeze(target);
+        /*
+            모든 프로퍼티를 순회하며 재귀적으로 동결한다.
+            Object.keys 메서드는 객체 자신의 열거 가능한 프로퍼티 키를 배열로 반환한다.
+            forEach 메서드는 배열을 순회하며 배열의 각 요소에 대하여 콜백 함수를 실행한다.
+        */
+        Object.keys(target).forEach(key => deepFreeze(target[key]));
+    }
+    return target;
+}
+
+const person = {
+    name: 'Lee',
+    address: {city: 'Seoul'}
+};
+
+// 깊은 객체 동결
+deepFreeze(person);
+
+console.log(Object.isFrozen(person)); // true
+// 중첩 객체까지 동결한다.
+console.log(Object.isFrozen(person.address)); // true
+
+person.address.city = 'Busan';
+console.log(person); // {name: "Lee", address: {city: "Seoul"}}
