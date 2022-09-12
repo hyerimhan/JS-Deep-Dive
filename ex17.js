1cfdfcaf71722cb7b8508a8640566a71e1ec7df7
Hyerim Han <hyerim_han@naver.com>
2022-09-13 00:59:11 +0900

    생성자 함수에 의한 객체 생성

--- ex17.js
+++ ex17.js
@@ -124,13 +124,167 @@ const circle1 = new Circle(5); // 반지름이 5인 Circle 객체를 생성
 
 
 
-// ex17-08
+// ex17-08 ~ 17-10
 function Circle(radius) {
     // 1. 암묵적으로 인스턴스가 생성되고 this에 바인딩된다.
-    console.log(this); // Circle {}
+    // console.log(this); // Circle {}
+
+    // 2. this에 바인딩되어 있는 인스턴스를 초기화한다.
+    this.radius = radius;
+    this.getDiameter = function() {
+        return 2 * this.radius;
+    };
+
+    // 3. 완성된 인스턴스가 바인딩된 this가 암묵적으로 반환된다.
+}
+
+// 인스턴스 생성. Circle 생성자 함수는 암묵적으로 this를 반환한다.
+const circle = new Circle(1);
+console.log(circle); // Circle {radius: 1, getDiameter: f}
+
+
+
+// ex17-11
+function Circle(radius) {
+    // 1. 암묵적으로 인스턴스가 생성되고 this에 바인딩된다.
+    // console.log(this); // Circle {}
+
+    // 2. this에 바인딩되어 있는 인스턴스를 초기화한다.
+    this.radius = radius;
+    this.getDiameter = function() {
+        return 2 * this.radius;
+    };
+
+    // 3. 암묵적으로 this를 반환된다.
+    // 명시적으로 객체를 반환하면 암묵적인 this 반환이 무시된다.
+    return {};
+}
+
+// 인스턴스 생성. Circle 생성자 함수는 명시적으로 반환한 객체를 반환한다.
+const circle = new Circle(1);
+console.log(circle); // {}
+
+
+
+// ex17-12
+function Circle(radius) {
+    // 1. 암묵적으로 인스턴스가 생성되고 this에 바인딩된다.
+    // console.log(this); // Circle {}
+
+    // 2. this에 바인딩되어 있는 인스턴스를 초기화한다.
+    this.radius = radius;
+    this.getDiameter = function() {
+        return 2 * this.radius;
+    };
+
+    // 3. 암묵적으로 this를 반환된다.
+    // 명시적으로 원시 값을 반환하면 원시 값 반환은 무시되고 암묵적으로 this가 반환된다.
+    return 100;
+}
+
+// 인스턴스 생성. Circle 생성자 함수는 명시적으로 반환한 객체를 반환한다.
+const circle = new Circle(1);
+console.log(circle); // Circle {radius: 1, getDiameter: f}
+// 생성자 함수 내부에서 명시적으로 this가 아닌 다른 값을 반환하는 것은 생성자 함수의 기본동작을 훼손한다.
+// 따라서 생성자 함수 내부에서 return 문은 반드시 생략해야 한다.
+
+
+
+// ex17-15
+// 일반 함수 정의: 함수 선언문, 함수 표현식
+function foo() {}
+const bar = function() {};
+// 프로퍼티 x의 값으로 할당된 것은 일반 함수로 정의된 함수다. 이는 메서드로 인정하지 않는다.
+const baz = {
+    x: function() {}
+};
+
+// 일반 함수로 정의된 함수만이 constructor(생성자 함수)다.
+new foo(); // foo {}
+new bar(); // bar {}
+new baz.x(); // x {}
+
+// 화살표 함수 정의
+const arrow = () => {};
+
+new arrow(); // TypeError: arrow is not a constructor
+
+// 메서드 정의: ES6의 메서드 축약 표현만 메서드로 인정한다.
+const obj = {
+    x() {}
+};
+
+new obj.x(); // TypeError: obj.x is not a constructor
+// 일반 함수(함수 선언문, 함수 표현식)으로 정의된 함수만이 constructor이고 ES6의 화살표 함수와 메서드 축약 표현으로 정의된 함수는 non-constructor이다.
+
+
+
+// ex17-17
+// new연산자
+// 생성자 함수로서 정의하지 않은 일반 함수
+function add(x, y) {
+    return x + y;
+}
+
+// 생성자 함수로서 정의하지 않은 일반 함수를 new 연산자와 함께 호출
+let inst = new add();
+
+// 함수가 객체를 반환하지 않았으므로 반환문이 무시된다. 따라서 빈 객체가 생성되어 반환된다.
+console.log(inst); // {}
+
+// 객체를 반환하는 일반 함수
+function createUser(name, role) {
+    return {name, role};
+}
+
+// 일반 함수를 new 연산자와 함께 호출
+inst = new createUser('Lee', 'admin');
+// 함수가 생성한 객체를 반환한다.
+console.log(inst); // {name: "Lee", role: "admin"}
+
+
+
+// ex17-18
+// new 연산자 없이 생성자 함수를 호출하면 일반 함수로 호출된다.
+// 생성자 함수
+function Circle(radius) {
+    this.radius = radius;
+    this.getDiameter = function() {
+        return 2 * this.radius;
+    };
+}
+
+// new 연산자 없이 생성자 함수 호출하면 일반 함수로서 호출된다.
+const circle = Circle(5);
+console.log(circle); // undefined
+
+// 일반 함수 내부의 this는 전역 객체 window를 가리킨다.
+console.log(radius); // 5
+console.log(getDiameter()); // 10
+
+circle.getDiameter(); // TypeError: Cannot read property 'getDiameter' of undefined
+// new 연산자와 함께 생성자 함수로서 호출하면 함수 내부의 this는 Circle 생성자 함수가 생성할 인스턴스를 가리킨다.
+// 하지만 Circle 함수를 일반적인 함수로서 호출하면 함수 내부의 this는 전역 객체 window를 가리킨다.
+// 생성자 함수는 일반적으로 첫 문자를 대문자로 기술하는 파스칼 케이스로 명명하여 일반 함수와 구별할 수 있도록 한다.
+
+
+
+// ex17-19
+// new.target은 IE에서는 지원하지 않음.
+// new 연산자와 함꼐 생성자 함수로서 호출되면 함수 내부의 new.target은 함수 자신을 가리킨다. new 연산지 없이 일반 함수로서 호출된 함수 내부의 new.target은 undefined다.
+// 생성자 함수
+function Circle(radius) {
+    if(!new.target) {
+        // new 연산자와 함께 생성자 함수를 재귀 호출하여 생성된 인스턴스를 반환한다.
+        return new Circle(radius);
+    }
 
     this.radius = radius;
     this.getDiameter = function() {
         return 2 * this.radius;
     };
 }
+
+// new 연산자 없이 생성자 함수를 호출하여도 new.target을 통해 생성자 함수로서 호출된다.
+const circle = Circle(5);
+console.log(circle.getDiameter());
